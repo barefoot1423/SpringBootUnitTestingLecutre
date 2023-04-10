f25522ad167f7f205659fa9da1d340ee9d0f2436
Hyunwoo Cho <barefoot1423@gmail.com>
2023-04-09 23:02:35 -0400

    restructuring codeˆ

--- src/test/java/com/hyunwoo/DemoUtilsTest.java
+++ src/test/java/com/hyunwoo/DemoUtilsTest.java
@@ -1,6 +1,7 @@
 package com.hyunwoo;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class DemoUtilsTest {
@@ -9,12 +10,8 @@ class DemoUtilsTest {
     void testEqualsAndNotEquals() {
         DemoUtils demoUtils = new DemoUtils();
 
-        int expected = 6;
-        int unexpected = 8;
-
-        int actual = demoUtils.add(2, 4);
-
-        assertEquals(expected, actual, "2 + 4 must be 6");
-        assertNotEquals(unexpected, actual, "2 + 4 must not be 8");
+        assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
+        assertNotEquals(8, demoUtils.add(2, 4), "2 +4 must not be 8");
     }
+
 }
