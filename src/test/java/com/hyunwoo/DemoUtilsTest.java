43e3265ea58b7e2b393f66434c2c784c3f8d39de
Hyunwoo Cho <barefoot1423@gmail.com>
2023-04-11 22:00:09 -0400

    @BeforeEach annotationˆ

--- src/test/java/com/hyunwoo/DemoUtilsTest.java
+++ src/test/java/com/hyunwoo/DemoUtilsTest.java
@@ -2,22 +2,27 @@ package com.hyunwoo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
-
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
         assertNotEquals(8, demoUtils.add(2, 4), "2 +4 must not be 8");
     }
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
-
         String str1 = null;
         String str2 = "luv2code";
 
