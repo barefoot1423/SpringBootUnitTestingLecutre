eb5bf38a9cafe2003dc4af9011e85279cefb3160
Hyunwoo Cho <barefoot1423@gmail.com>
2023-04-12 21:58:51 -0400

    @DisplayName

--- src/test/java/com/hyunwoo/DemoUtilsTest.java
+++ src/test/java/com/hyunwoo/DemoUtilsTest.java
@@ -30,6 +30,7 @@ class DemoUtilsTest {
     }
 
     @Test
+    @DisplayName("Null and Not Null")
     void testEqualsAndNotEquals() {
         System.out.println("Running test : testEqualsAndNotEquals");
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
@@ -37,6 +38,7 @@ class DemoUtilsTest {
     }
 
     @Test
+    @DisplayName("Equals and Not Equals")
     void testNullAndNotNull() {
         String str1 = null;
         String str2 = "luv2code";
